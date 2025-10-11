2bc823e01b32ccb14ca0c45e9c2b85918c3135bb
ko-tarou <tkota6174@gmail.com>
2025-10-12 03:02:54 +0900

    add ログイン画面のデザイン完成

--- ios/iOS/Config/APIConfig.swift
+++ ios/iOS/Config/APIConfig.swift
@@ -14,7 +14,7 @@ enum Environment {
     var baseURL: String {
         switch self {
         case .development:
-            return "https://5a6abf98d0e8.ngrok-free.app"
+            return "https://296bd24e2dff.ngrok-free.app"
         case .production:
             return "https://api.wincook.com" // 本番環境のURL
         }
