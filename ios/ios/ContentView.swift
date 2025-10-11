e3e8bb64105877df3ffa2196c6b29c8f03f03d3e
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-10-11 10:28:17 +0900

    タブ追加

--- ios/ios/ContentView.swift
+++ ios/ios/ContentView.swift
@@ -2,7 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HomeView()
+        @State var selection = 0
+        
+        TabView(selection: $selection) {
+            HomeView()   // ホーム画面
+                .tabItem {
+                    Label("Page1", systemImage: "house")
+                }
+                .tag(0)
+
+            RecipeView()   // レシピ詳細画面
+                .tabItem {
+                    Label("Page2", systemImage: "book.pages.fill")
+                }
+                .tag(1)
+            }
     }
 }
 
