27a7a540f63ba1ce9a5e9ac7e302f1ff3c738582
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-10-11 09:08:47 +0900

    first commit

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by 三ツ井渚 on 2025/10/11.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
